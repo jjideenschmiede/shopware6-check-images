2aef7105a3fe7e95aef15322a1c4d270c2697ce0
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-04-15 14:28:04 +0200

    Add site scraper

--- scraper/config.go
+++ scraper/config.go
@@ -10,3 +10,60 @@
 //**********************************************************
 
 package scraper
+
+import (
+	"github.com/gocolly/colly"
+)
+
+// Data struct to save data
+type Data struct {
+	Mpn  string `json:"mpn"`
+	Link string `json:"link"`
+}
+
+// Site ist for scraping the hole site content
+func Site(url string) ([]Data, bool) {
+
+	// Define variable to return
+	var data []Data
+
+	// To check last site
+	exits := false
+
+	// Create colly
+	c := colly.NewCollector()
+
+	// Check last site
+	c.OnHTML(".cms-listing-col", func(e *colly.HTMLElement) {
+
+		// Check last site
+		if e.DOM.Find(".alert-content-container").Children().HasClass("alert-content") {
+
+			// Return error
+			exits = true
+
+		}
+
+		// Check placeholder
+		if e.DOM.Find(".product-image-placeholder").Children().HasClass("icon-placeholder") {
+
+			// Save mpn to product
+			mpn, _ := e.DOM.Find("meta[itemprop=mpn]").Attr("content")
+
+			// Save link to product
+			link, _ := e.DOM.Find(".product-image-link").Attr("href")
+
+			// Add to return
+			data = append(data, Data{mpn, link})
+
+		}
+
+	})
+
+	// Visit site
+	c.Visit(url)
+
+	// Return values
+	return data, exits
+
+}
